e5ed37e61770cd908d57f9ce3227377218cb30de
thinhlq@hinnova <thinhlq@hinnova.com.vn>
2021-03-02 16:28:41 +0700

    add cyoa/refactoring project

--- cyoa/main.go
+++ cyoa/main.go
@@ -3,10 +3,37 @@ package main
 import (
 	"flag"
 	"fmt"
+	"html/template"
+	"log"
+	"net/http"
 	"os"
+	"strings"
 
 	b "example.com/main/cyoa/story"
 )
+
+var defaultHandlerTmpl = `
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>Choose Your Own Adventure</title>
+    </head>
+    <body>
+        <h1>{{.Title}}</h1>
+        {{range .Stories}}
+        <p>{{.}}</p>
+        {{end}}
+        <ul>
+            {{range .Options}}
+            <li>
+                <a href="/{{.Arc}}">{{.Text}}</a>
+            </li>
+			{{end}}
+        </ul>
+    </body>
+</html>
+`
 
 // //Story type
 // type Story map[string]Chapter
@@ -34,6 +61,7 @@ import (
 // }
 
 func main() {
+	port := flag.Int("port", 3000, "the port to start CYOA web application on")
 	filename := flag.String("file", "gophers.json", "The JSON with the CYOA story")
 	flag.Parse()
 
@@ -41,12 +69,32 @@ func main() {
 	if err != nil {
 		panic(err)
 	}
-	defer fd.Close()
+	//defer fd.Close()
 
-	story, err := b.JsonStory(fd)
+	story, err := b.JSONStory(fd)
 	if err != nil {
 		panic(err)
 	}
-	fmt.Printf("%+v\n", story)
 
+	tpl := template.Must(template.New("").Parse(defaultHandlerTmpl))
+
+	h := b.NewHandler(story,
+		b.WithTemplate(tpl),
+		b.WithPathFunc(pathFn),
+	)
+
+	mux := http.NewServeMux()
+	mux.Handle("/story/", h)
+	mux.Handle("/", b.NewHandler(story))
+	fmt.Printf("Starting the server at port: %d\n", *port)
+	//fmt.Printf("%+v\n", story)
+	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", *port), mux))
+}
+
+func pathFn(r *http.Request) string {
+	path := strings.TrimSpace(r.URL.Path)
+	if path == "" || path == "/story/" {
+		path = "/story/intro"
+	}
+	return path[len("/story"):]
 }
