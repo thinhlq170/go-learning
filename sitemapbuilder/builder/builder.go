24b5be2c154920d921418c300d1970900ef09acd
thinhlq <thinh.lq170@gmail.com>
2021-03-10 00:02:34 +0700

    [Sitemap Builder] finish

--- sitemapbuilder/builder/builder.go
+++ sitemapbuilder/builder/builder.go
@@ -3,6 +3,8 @@ package builder
 import (
 	"encoding/xml"
 	"io"
+	"net/http"
+	"net/url"
 	"strings"
 
 	"example.com/main/htmllinkparser/parser"
@@ -33,4 +35,86 @@ func LinkBuilder(page string, linksInPage []parser.Link, writtenFile io.Writer) 
 		}
 	}
 	return errResult
+}
+
+//Mybfs uses bfs algorithm to search all links from the page "urlStr" and returns slice of string
+func Mybfs(urlStr string, maxDepth int) []string {
+
+	seen := make(map[string]struct{})
+	var q map[string]struct{}
+	nq := map[string]struct{}{
+		urlStr: {},
+	}
+
+	for i := 0; i < maxDepth+1; i++ {
+		q, nq = nq, make(map[string]struct{})
+		for url := range q {
+			if _, ok := seen[url]; ok {
+				continue
+			}
+			seen[url] = struct{}{}
+			links := getURL(url)
+			for _, link := range links {
+				nq[link] = struct{}{}
+			}
+		}
+	}
+
+	ret := make([]string, 0, len(seen))
+	for link := range seen {
+		ret = append(ret, link)
+	}
+	return ret
+}
+
+func getURL(urlStr string) []string {
+	res, err := http.Get(urlStr)
+	if err != nil {
+		panic(err)
+	}
+	reqURL := res.Request.URL
+	baseURL := &url.URL{
+		Scheme: reqURL.Scheme,
+		Host:   reqURL.Host,
+	}
+	base := baseURL.String()
+	return filter(hrefs(res.Body, base), withPrefixFn(base))
+}
+
+func hrefs(r io.Reader, base string) []string {
+	var ret []string
+
+	links, err := parser.LinkParser(r)
+	if err != nil {
+		panic(err)
+	}
+
+	for _, link := range links {
+		switch {
+		case strings.HasPrefix(link.Href, "/"):
+			ret = append(ret, base+link.Href)
+		case strings.HasPrefix(link.Href, "http"):
+			ret = append(ret, link.Href)
+		}
+	}
+
+	return ret
+}
+
+func filter(links []string, checkPrefixFn func(string) bool) []string {
+	var ret []string
+
+	for _, link := range links {
+		if checkPrefixFn(link) {
+			ret = append(ret, link)
+		}
+	}
+
+	return ret
+}
+
+func withPrefixFn(pfx string) func(string) bool {
+	return func(link string) bool {
+		return strings.HasPrefix(link, pfx)
+	}
 }
--- sitemapbuilder/main.go
+++ sitemapbuilder/main.go
@@ -1,13 +1,13 @@
 package main
 
 import (
+	"encoding/xml"
+	"flag"
 	"fmt"
 	"log"
-	"net/http"
 	"net/url"
 	"os"
 
-	"example.com/main/htmllinkparser/parser"
 	"example.com/main/sitemapbuilder/builder"
 )
 
@@ -15,6 +15,17 @@ func check(e error) {
 	if e != nil {
 		log.Fatal(e)
 	}
+}
+
+const xmlns = "http://www.sitemaps.org/schemas/sitemap/0.9"
+
+type loc struct {
+	Value string `xml:"loc"`
+}
+
+type urlset struct {
+	Urls  []loc  `xml:"url"`
+	Xmlns string `xml:"xmlns,attr"`
 }
 
 func main() {
@@ -25,10 +36,37 @@ func main() {
 	_, err := url.ParseRequestURI(page)
 	check(err)
 
-	response, err := http.Get(page)
+	urlFlag := flag.String("url", page, "the url that you want to build a sitemap for")
+	maxDepthFlag := flag.Int("maxDepth", 10, "the maximum number of links deep to traverse")
+	flag.Parse()
 
-	links, err := parser.LinkParser(response.Body)
+	//response, err := http.Get(page)
 
-	errRes := builder.LinkBuilder(page, links, os.Stdout)
-	check(errRes)
+	//links, err := parser.LinkParser(response.Body)
+
+	// errRes := builder.LinkBuilder(page, links, os.Stdout)
+	// check(errRes)
+
+	type urlBuilder struct {
+		XMLName xml.Name `xml:"url"`
+		LOC     string   `xml:"loc"`
+	}
+
+	toXML := urlset{
+		Xmlns: xmlns,
+	}
+
+	links := builder.Mybfs(*urlFlag, *maxDepthFlag)
+	//linksBuilder := make([]urlBuilder, 0, len(links))
+	for _, link := range links {
+		//linksBuilder = append(linksBuilder, urlBuilder{LOC: link})
+		toXML.Urls = append(toXML.Urls, loc{Value: link})
+	}
+
+	fmt.Printf(xml.Header)
+	enc := xml.NewEncoder(os.Stdout)
+	enc.Indent("", "  ")
+	if err := enc.Encode(toXML); err != nil {
+		panic(err)
+	}
 }
